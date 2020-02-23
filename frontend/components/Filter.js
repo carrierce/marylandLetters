6b52db3fd1151264e8e2fa493be04c0b7617466d
Charles Carrier <charles.e.carrier@gmail.com>
2020-02-23 22:05:29 +0700

    feb 23 2020 starting styling filters

--- frontend/components/Filter.js
+++ frontend/components/Filter.js
@@ -1,8 +1,12 @@
 const Filter = props => {
   const filterName = props.filterName;
+  const upperCaseFilterName =
+    filterName.charAt(0).toUpperCase() + filterName.slice(1);
   return (
-    <div className="container m-4 border-b-2 border-grey pb-5">
-      <label>{filterName}</label>
+    <div
+      className={`container m-4 border-b-2 border-grey pb-5 ${props.filterBackground}`}
+    >
+      <label className="mr-2">{upperCaseFilterName}</label>
       <input
         type="checkbox"
         value={props.filterObject[filterName].enabled}
--- frontend/components/Filters.js
+++ frontend/components/Filters.js
@@ -1,29 +1,36 @@
 import Filter from "./Filter";
 
 const Filters = props => {
+  //   const filters = [
+  //     "year",
+  //     "day",
+  //     "month",
+  //     "wordCount",
+  //     "fromFirstName",
+  //     "fromLastName",
+  //     "notes",
+  //     "openingNote",
+  //     "place",
+  //     "postmark",
+  //     "text",
+  //     "toAddress",
+  //     "toPersonFromPerson",
+  //     "to"
+  //   ];
   const filters = [
-    "year",
-    "day",
-    "month",
-    "wordCount",
-    "fromFirstName",
-    "fromLastName",
-    "notes",
-    "openingNote",
-    "place",
-    "postmark",
-    "text",
-    "toAddress",
-    "toPersonFromPerson",
-    "to"
+    { name: "year", background: "bg-gray-200" },
+    { name: "day", background: "bg-red-200" },
+    { name: "month", background: "bg-orange-200" },
+    { name: "wordCount", background: "bg-yellow-200" }
   ];
   return (
     <form onSubmit={props.handleSubmit}>
-      {filters.map(data => {
+      {filters.map(filterName => {
         return (
           <Filter
-            key={data}
-            filterName={data}
+            key={filterName.name}
+            filterName={filterName.name}
+            filterBackground={filterName.background}
             filterObject={props.filter}
             handleInput={props.handleInput}
             handleCheckbox={props.handleCheckbox}

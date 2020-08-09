1ca4523a4f54cf5cb325d9e8304bdc6f293bb658
Charles Carrier <charles.e.carrier@gmail.com>
2020-08-09 18:51:05 +0800

    updating PORT

--- expressblogapi/index.js
+++ expressblogapi/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const appRoot = require('app-root-path');
 const app = express();
 const letterRoutes = require(appRoot + '/routes/letters');
-const PORT = process.env.PORT || 5500;
+const PORT = 5500;
 const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 app.use('/api', letterRoutes);
-app.use(function(req, res) {
+app.use(function (req, res) {
   res.send('404: Page not Found');
 });
 
